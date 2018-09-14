83d3ec50b22f560bcf6f0116b31715323639f89f
Jen-Chieh Chiang <jchiang36@gatech.edu>
2018-09-14 00:05:41 -0400

    implemented route for current user's profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// load profile model
+const Profile = require('../../models/Profile');
+// loda user profile
+const User = require('../../models/User');
 
 
 // @route   GET api/profile/test
@@ -7,4 +14,22 @@ const router = express.Router();
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: 'Profile works'}));
 
+
+// @route   GET api/profile/
+// @desc    get current user's profile
+// @access  Private
+router.get('/', 
+  passport.authenticate('jwt', { session: false}), 
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({user: req.user.id})
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(400).json(err));
+  });
 module.exports = router;
