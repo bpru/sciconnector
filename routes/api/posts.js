8712228b51ff279120478abad39c8472aa7074ba
Jen-Chieh Chiang <jchiang36@gatech.edu>
2018-09-24 02:24:22 -0400

    implemented like and unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -72,4 +72,39 @@ router.delete('/:id', passport.authenticate('jwt', { session: false}), (req, res
     })
   })
 })
+
+// @route   POST api/posts/like/:id
+// @desc    like post
+// @access  Private
+router.post('/like/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id}).then(profile => {
+    Post.findById(req.params.id).then(post => {
+      if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+        return res.status(400).json({alreadyliked: 'User has alreayd liked this post'});
+      }
+      // add user id to likes array
+      post.likes.unshift({user: req.user.id});
+      post.save().then(post => res.json(post));
+    }).catch( err => res.status(404).json({postnotfound: 'No post found'}));
+  });
+});
+
+// @route   POST api/posts/unlike/:id
+// @desc    unlike post
+// @access  Private
+router.post('/unlike/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id}).then(profile => {
+    Post.findById(req.params.id).then(post => {
+      if (post.likes.filter(post => post.user.toString() === req.user.id).length === 0) {
+        return res.status(400).json({notliked: 'You have not liked this post'});
+      }
+      // find like index
+      const removeIdx = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+      // remove like
+      post.likes.splice(removeIdx, 1);
+      // save
+      post.save().then(post => res.json(post));
+    }).catch(err => { res.status(404).json({postnotfound: 'No post found'})});
+  });
+});
 module.exports = router;
