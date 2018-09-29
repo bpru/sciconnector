7e1379f3e8ff0f948b05400d7b2ec3699298871d
Jen-Chieh Chiang <jchiang36@gatech.edu>
2018-09-29 01:47:21 -0400

    restructre input fields to TextFieldGroup component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 import {loginUser} from "../../actions/authActions";
+import TextFieldGroup from '../common/TextFieldGroup';
 
 
 class Login extends Component {
@@ -47,24 +48,20 @@ class Login extends Component {
 							<h1 className="display-4 text-center">Log In</h1>
 							<p className="lead text-center">Sign in to your DevConnector account</p>
 							<form onSubmit={this.onSubmit}>
-								<div className="form-group">
-									<input type="email"
-									       className={classnames("form-control form-control-lg", {'is-invalid': errors.email})}
-									       placeholder="Email Address"
-									       value={this.state.email}
-									       onChange={this.onChange}
-									       name="email"/>
-									{errors.email && <div className='invalid-feedback'>{errors.email}</div>}
-								</div>
-								<div className="form-group">
-									<input type="password"
-									       className={classnames("form-control form-control-lg", {'is-invalid': errors.password})}
-									       placeholder="Password"
-									       value={this.state.password}
-									       onChange={this.onChange}
-									       name="password"/>
-									{errors.password && <div className="invalid-feedback">{errors.password}</div>}
-								</div>
+								<TextFieldGroup
+									onChange={this.onChange}
+									name='email'
+									type='email'
+									value={this.state.email}
+									placeholder='Email Address'
+									error={errors.email}/>
+								<TextFieldGroup
+									onChange={this.onChange}
+									name='password'
+									type='password'
+									value={this.state.password}
+									placeholder='Password'
+									error={errors.password}/>
 								<input type="submit"
 								       className="btn btn-info btn-block mt-4"/>
 							</form>
