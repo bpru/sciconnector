3dd4720740cdd551cc1b76ac04e7ccb1d0fac0be
Jen-Chieh Chiang <jchiang36@gatech.edu>
2018-10-04 11:16:10 -0400

    prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  // mongoURI: 'mongodb://bpru:Password01@ds151602.mlab.com:51602/devconnector',
-  mongoURI: 'mongodb://localhost',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 
@@ -31,6 +32,16 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+//server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+//	set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 
 const port = process.env.PORT || 5000; //process.env.PORT is for developing to heroku
 
